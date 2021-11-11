d9f7a7d3e4aea3bc8ff0d0a8b8378dffb1845da1
JSaturn9 <69169169+JSaturn9@users.noreply.github.com>
2021-11-11 15:22:29 -0500

    Getting started on front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, Chat!</h1>
+      <h1 id="greeting">Welcome to React Chat!</h1>
+      <div id="room-wrapper">
+        <div id="main-room">
+          <h2>Main Room</h2>
+          <p>Chats will go here</p>
+          <p>Chats will go here</p>
+          <p>Chats will go here</p>
+        </div>
+        <div id="all-rooms">
+          <h2>All Rooms</h2>
+          <h3>Dog Room</h3>
+          <h3>Cat Room</h3>
+          <h3>Bird Room</h3>
+        </div>
+      </div>
+      <div id="form-container">
+        <form method="post">
+          <div id="inputs-wrapper">
+          <input type="text" placeholder="Enter username" id="username-field" />
+          <textarea type="text" placeholder="Enter message"  id="message-field" />
+          </div>
+          <input type="submit" value="Send" className="button" />
+          <input type="submit" value="Refresh" className="button" />
+          
+        </form>
+      </div>
     </div>
   );
 }
