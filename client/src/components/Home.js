33b8b2696f49d05c09482a103fdf9a367a11f271
dillerwin <dillerwin@gmail.com>
2021-11-12 15:51:15 -0500

    working chat window!

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,32 @@
 //Imports Link from react-router-dom to allow for links in component
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 //Function for home page
 function Home(props) {
+  const [username, setUserName] = useState("");
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api/main-chat")
+      .then((res) => res.json())
+      .then((homeData) => {
+        let temp = homeData.map((item) => {
+          return (
+            <li>
+              {item.username}: {item.message}
+            </li>
+          );
+        });
+        setMessage(temp);
+      });
+  }, []);
+
+  function clickHandle(event) {
+    event.preventDefault();
+    setMessage(`hello`);
+  }
+
   //Returns chat room page
   return (
     <div>
@@ -11,9 +35,7 @@ function Home(props) {
         <div className="main-room">
           <h2>Main Room</h2>
           {/* Eventually pushed up chats will go into the p tag (maybe append li instead?)*/}
-          <p>Chats will go here</p>
-          <p>Chats will go here</p>
-          <p>Chats will go here</p>
+          <p name="chat">{message}</p>
         </div>
         <div className="all-rooms">
           <h2>All Rooms</h2>
@@ -33,22 +55,30 @@ function Home(props) {
       </div>
       {/* Form for user name and message inputs */}
       <div className="form-container">
-        <form method="post">
+        <form method="post" action="/main-chat">
           <div className="inputs-wrapper">
             <input
+              name="username"
               type="text"
               placeholder="Enter username"
               className="username-field"
             />
             <textarea
+              name="message"
               type="text"
               placeholder="Enter message"
               className="message-field"
             />
+            <input type="submit" value="Send" className="button" />
           </div>
-          {/* Send and refresh buttons linked to server */}
-          <input type="submit" value="Send" className="button" />
-          <input type="submit" value="Refresh" className="button" />
+        </form>
+        <form method="get" action="api/main-chat">
+          <input
+            name="button"
+            type="button"
+            value="Refresh"
+            onClick={clickHandle}
+          />
         </form>
       </div>
     </div>
--- server.js
+++ server.js
@@ -1,18 +1,50 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const db = mongoose.connection;
 
 const port = process.env.PORT || 8000;
 const app = express();
 const staticDir = process.env.DEV ? "./client/public" : "./client/build";
 
-const url = `mongodb+srv://${process.env.username}:${process.env.password}@react-chat-dillon-jo.nfgg1.mongodb.net/test`;
+const url = `mongodb+srv://${process.env.user}:${process.env.password}@react-chat-dillon-jo.nfgg1.mongodb.net/react-chat`;
 
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.static(staticDir));
-
-
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-  console.log('listening on port: ' + port) 
-})
+  console.log("listening on port: " + port);
+});
+
+const chatSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  message: { type: String, required: true },
+});
+
+const Chat = mongoose.model("Chat", chatSchema);
+
+app.get("/dog-room", (req, res) => {});
+
+app.get("/cat-room", (req, res) => {});
+
+app.get("/bird-room", (req, res) => {});
+
+app.post("/main-chat", async (req, res) => {
+  console.log(`post to chat`);
+
+  let userObj = req.body;
+
+  let newChat = new Chat(userObj);
+  await newChat.save();
+  res.redirect("/");
+});
+
+let temp;
+
+app.get("/api/main-chat", async (req, res) => {
+  let allChats = await Chat.find({});
+  res.send(allChats);
+});
