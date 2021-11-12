2d3932b82acf00b195dce38b4f9d3965768dbd25
JSaturn9 <69169169+JSaturn9@users.noreply.github.com>
2021-11-12 15:52:54 -0500

    Worked on client-side functionality

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,27 @@
 //Imports Link from react-router-dom to allow for links in component
 import { Link } from "react-router-dom";
+//Imports useEffect and useState to be used in component
+import { useState, useEffect } from "react";
 
 //Function for home page
 function Home(props) {
+  //Sets the chat username
+  const [username, setUserName] = useState("");
+  //Sets the chat message
+  const [message, setMessage] = useState("");
+
+  //useEffect hook to pull in info from the database (json?)
+  useEffect(() => {
+    fetch("/api/main-chat")
+      .then((res) => res.json())
+      .then((homeData) => {
+        homeData.forEach((doc) => {
+          setUserName(doc.username);
+          setMessage(doc.message);
+        });
+      });
+  }, []);
+
   //Returns chat room page
   return (
     <div>
@@ -10,10 +29,12 @@ function Home(props) {
       <div className="room-wrapper">
         <div className="main-room">
           <h2>Main Room</h2>
-          {/* Eventually pushed up chats will go into the p tag (maybe append li instead?)*/}
-          <p>Chats will go here</p>
-          <p>Chats will go here</p>
-          <p>Chats will go here</p>
+          {/* Eventually pushed up chats will go into the li tags */}
+          <ul>
+            <li>
+              {username} : {message}
+            </li>
+          </ul>
         </div>
         <div className="all-rooms">
           <h2>All Rooms</h2>
@@ -33,22 +54,23 @@ function Home(props) {
       </div>
       {/* Form for user name and message inputs */}
       <div className="form-container">
-        <form method="post">
+        <form action="/main-chat" method="post">
           <div className="inputs-wrapper">
             <input
               type="text"
+              name="username"
               placeholder="Enter username"
               className="username-field"
             />
             <textarea
               type="text"
+              name="message"
               placeholder="Enter message"
               className="message-field"
             />
+            {/* Send button linked to server */}
+            <input type="submit" value="Send" className="button" />
           </div>
-          {/* Send and refresh buttons linked to server */}
-          <input type="submit" value="Send" className="button" />
-          <input type="submit" value="Refresh" className="button" />
         </form>
       </div>
     </div>
--- server.js
+++ server.js
@@ -1,18 +1,47 @@
+//Imports dotenv into file
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 
+//creates a variable to initialize port
 const port = process.env.PORT || 8000;
+//Express server setup
 const app = express();
 const staticDir = process.env.DEV ? "./client/public" : "./client/build";
 
-const url = `mongodb+srv://${process.env.username}:${process.env.password}@react-chat-dillon-jo.nfgg1.mongodb.net/test`;
-
+//
+const url = `mongodb+srv://${process.env.user}:${process.env.password}@react-chat-dillon-jo.nfgg1.mongodb.net/react-chat`;
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.static(staticDir));
+app.use(express.urlencoded({ extended: true }));
 
+const chatSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  message: { type: String, required: true },
+});
 
+const Chat = mongoose.model("Chat", chatSchema);
+
+//CREATE - send messages on the home route to the chat box
+app.post("/main-chat", async (req, res) => {
+  let userObj = req.body
+  let newChat = new Chat(userObj);
+  await newChat.save();
+  res.redirect("/");
+});
+
+app.get("/api/main-chat", async (req, res) => {
+  let allChats = await Chat.find({})
+  res.send(allChats)
+})
+
+//sets a 10 second timeout on the chat
+setTimeout(() => {
+  
+}, 10000);
 
 app.listen(port, () => {
-  console.log('listening on port: ' + port) 
-})
+  console.log("listening on port: " + port);
+});
